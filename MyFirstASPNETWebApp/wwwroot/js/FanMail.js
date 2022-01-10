5529c2d140923805ff6f284c9156665b2b797ae4
JacksonSchow <80366532+JacksonSchow@users.noreply.github.com>
2022-01-09 18:43:41 -0700

    Initial check-in of module MyFirstASPNETWebApp

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,7 @@
+﻿$("#buttonsend").click(function () {
+    alert("From: \n" + $("#txtFrom").val() + "\n" +
+        "Subject: \n" + $("#txtSubject").val() + "\n" +
+        "Message: \n" + $("#txtMessage").val());
+
+    $("#picganderson").fadeToggle("slow");
+})
